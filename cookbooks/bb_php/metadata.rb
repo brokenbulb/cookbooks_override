0adf8c7f59a4382340abf19febbccd25f119a03f
Matt Parrett <matt.parrett@gmail.com>
2012-08-07 13:37:50 -0500

    added ssh_keyscan recipe

--- cookbooks/bb_php/metadata.rb
+++ cookbooks/bb_php/metadata.rb
@@ -5,7 +5,16 @@ description      "Installs/Configures bb_php"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
 recipe "bb_php::default","Configures BB PHP Environment"
 recipe "bb_php::install_private_key","Installs private key"
 recipe "bb_php::install_public_key","Installs public key"
 recipe "bb_php::httpd_graceful","Gracefully restart httpd"
+recipe "bb_php::ssh_keyscan","Add ssh host to known_hosts"
+
+attribute "bb_php/ssh_keyscan_host",
+  :display_name => "Host to add",
+  :description => "Host to scan and add to known_hosts",
+  :required => "required",
+  :recipes => ["bbphp::ssh_keyscan"]
---
+++ cookbooks/bb_php/recipes/ssh_keyscan.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: bb_php
+# Recipe:: ssh_keyscan 
+#
+# Copyright 2012, Broken Bulb Studios
+#
+# All rights reserved - Do Not Redistribute
+#
+# Add ssh host to known_hosts 
+
+execute "SSH Keyscan" do
+	command "ssh-keyscan -t rsa,dsa " + node[:bb_php][:ssh_keyscan_host] + " 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts && cat ~/.ssh/tmp_hosts > ~/.ssh/known_hosts"
+end

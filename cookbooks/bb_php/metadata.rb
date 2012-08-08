a8c38829d13fb8d508455171b587eeac25bf70ce
Matt Parrett <matt.parrett@gmail.com>
2012-08-08 15:30:45 -0500

    removed rightscale dependency

--- cookbooks/bb_php/metadata.rb
+++ cookbooks/bb_php/metadata.rb
@@ -5,8 +5,6 @@ description      "Installs/Configures bb_php"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "rightscale"
-
 recipe "bb_php::default","Configures BB PHP Environment"
 recipe "bb_php::install_private_key","Installs private key"
 recipe "bb_php::install_public_key","Installs public key"

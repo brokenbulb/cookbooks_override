43cd2c4e2bf6262d1770105644f247dbbd80c343
Matt Parrett <matt.parrett@gmail.com>
2012-09-10 14:00:43 -0500

    added action

--- cookbooks/bb_php/recipes/git_clone.rb
+++ cookbooks/bb_php/recipes/git_clone.rb
@@ -13,4 +13,5 @@ execute "git_clone" do
   RightScale::Repo::Ssh_key.new.create(node[:repo][:default][:git_ssh_key])
   command "NEWDIR=/home/capistrano_repo/releases/`date +%s`; mkdir -p $NEWDIR; cd $NEWDIR; git clone git@github.com:brokenbulb/SuperheroesLive.git $NEWDIR"
   RightScale::Repo::Ssh_key.new.delete
+  action :run
 end

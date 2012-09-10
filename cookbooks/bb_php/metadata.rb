6c371e3dd1842ac9faab46fef15abe506a5aee7b
Matt Parrett <matt.parrett@gmail.com>
2012-09-10 12:27:15 -0500

    update metadat

--- cookbooks/bb_php/metadata.rb
+++ cookbooks/bb_php/metadata.rb
@@ -5,6 +5,8 @@ description      "Installs/Configures bb_php"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "repo"
+
 recipe "bb_php::default","Configures BB PHP Environment"
 recipe "bb_php::install_private_key","Installs private key"
 recipe "bb_php::install_public_key","Installs public key"
@@ -12,6 +14,7 @@ recipe "bb_php::httpd_graceful","Gracefully restart httpd"
 recipe "bb_php::ssh_keyscan","Add ssh host to known_hosts"
 recipe "bb_php::symlink_latest","Symlink latest code pull"
 recipe "bb_php::symlink_last","Symlink last-to-latest code pull"
+recipe "bb_php::git_clone","Clone the Superhero repository"
 
 attribute "bb_php/ssh_keyscan_host",
   :display_name => "Host to add",

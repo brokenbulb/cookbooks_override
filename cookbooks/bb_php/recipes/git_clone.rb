43f5d25c590646c8cd1c4c5004ae3d38cb7909dd
Matt Parrett <matt.parrett@gmail.com>
2012-09-19 12:32:22 -0500

    disable ETag and update git_clone script

--- cookbooks/bb_php/recipes/git_clone.rb
+++ cookbooks/bb_php/recipes/git_clone.rb
@@ -10,8 +10,39 @@
 #
 
 execute "git_clone" do
-  RightScale::Repo::Ssh_key.new.create(node[:repo][:default][:git_ssh_key])
-  command "NEWDIR=/home/capistrano_repo/releases/`date +%s`; mkdir -p $NEWDIR; cd $NEWDIR; git clone git@github.com:brokenbulb/SuperheroesLive.git $NEWDIR"
-  RightScale::Repo::Ssh_key.new.delete
+  keyfile = nil
+  ssh_key = node[:repo][:default][:git_ssh_key]
+  ssh_wrapper = ssh_key + ".sh"
+  reponame = node[:repo][:default][:repository]
+  
+  if "#{ssh_key}" != ""
+    keyfile = "/tmp/gitkey"
+    bash 'create_temp_git_ssh_key' do
+      code <<-EOH
+        echo -n '#{ssh_key}' > #{keyfile}
+        chmod 700 #{keyfile}
+        echo 'exec ssh -oStrictHostKeyChecking=no -i #{keyfile} "$@"' > #{ssh_wrapper}
+        chmod +x #{keyfile}.sh
+      EOH
+    end
+  end
+
+  bash 'git_clone' do
+    NEWDIR=/home/capistrano_repo/releases/`date +%s`
+    mkdir -p $NEWDIR
+    cd $NEWDIR
+    git clone #{reponame} $NEWDIR
+  end
+
+  # delete SSH key & clear GIT_SSH
+  if keyfile != nil
+     bash 'delete_temp_git_ssh_key' do
+       code <<-EOH
+         rm -f #{keyfile}
+         rm -f #{ssh_wrapper}
+       EOH
+     end
+  end
+
   action :run
 end

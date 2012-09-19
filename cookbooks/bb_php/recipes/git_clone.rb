cf64a1a5fa491b8834af7e57623debc7dcc8a21b
Matt Parrett <matt.parrett@gmail.com>
2012-09-19 14:36:41 -0500

    fixed unnecessary action statement, logging, rightscale markers

--- cookbooks/bb_php/recipes/git_clone.rb
+++ cookbooks/bb_php/recipes/git_clone.rb
@@ -9,41 +9,45 @@
 # Clone git repo 
 #
 
-execute "git_clone" do
-  keyfile = nil
-  ssh_key = node[:repo][:default][:git_ssh_key]
-  ssh_wrapper = ssh_key + ".sh"
-  reponame = node[:repo][:default][:repository]
-  
-  if "#{ssh_key}" != ""
-    keyfile = "/tmp/gitkey"
-    bash 'create_temp_git_ssh_key' do
-      code <<-EOH
-        echo -n '#{ssh_key}' > #{keyfile}
-        chmod 700 #{keyfile}
-        echo 'exec ssh -oStrictHostKeyChecking=no -i #{keyfile} "$@"' > #{ssh_wrapper}
-        chmod +x #{keyfile}.sh
-      EOH
-    end
-  end
+rightscale_marker :begin
 
-  bash 'git_clone' do
-    dest = "/home/capistrano_repo/releases/" + ::Time.now.to_i
+keyfile = nil
+ssh_key = node[:repo][:default][:git_ssh_key]
+ssh_wrapper = ssh_key + ".sh"
+reponame = node[:repo][:default][:repository]
+dest = "/home/capistrano_repo/releases/" + ::Time.now.to_i
+keyfile = "/tmp/gitkey"
+
+log "  Running BB git clone on #{reponame} to #{dest}"
+
+if "#{ssh_key}" != ""
+    bash 'create_temp_git_ssh_key' do
+        code <<-EOH
+            echo -n '#{ssh_key}' > #{keyfile}
+            chmod 700 #{keyfile}
+            echo 'exec ssh -oStrictHostKeyChecking=no -i #{keyfile} "$@"' > #{ssh_wrapper}
+            chmod +x #{keyfile}.sh
+        EOH
+    end
+end
+
+bash 'git_clone' do
     code <<-EOH
         mkdir -p #{dest} && cd #{dest}
         git clone #{reponame} #{dest} 
     EOH
-  end
-
-  # delete SSH key & clear GIT_SSH
-  if keyfile != nil
-     bash 'delete_temp_git_ssh_key' do
-       code <<-EOH
-         rm -f #{keyfile}
-         rm -f #{ssh_wrapper}
-       EOH
-     end
-  end
-
-  action :run
 end
+
+# delete SSH key & clear GIT_SSH
+if keyfile != nil
+    bash 'delete_temp_git_ssh_key' do
+        code <<-EOH
+            rm -f #{keyfile}
+            rm -f #{ssh_wrapper}
+        EOH
+    end
+end
+	
+log "  BB git clone done!" 
+
+rightscale_marker :end

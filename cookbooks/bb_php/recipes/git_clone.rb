17b29c66163622c3a5f78ee73ad68b8897b81c2f
Matt Parrett <matt.parrett@gmail.com>
2012-09-19 16:12:26 -0500

    Added logging

--- cookbooks/bb_php/recipes/git_clone.rb
+++ cookbooks/bb_php/recipes/git_clone.rb
@@ -15,9 +15,8 @@ ssh_key_file = nil
 ssh_key = node[:repo][:default][:git_ssh_key]
 reponame = node[:repo][:default][:repository]
 dest = "/home/capistrano_repo/releases/" + ::Time.now.strftime("%Y%m%d%H%M") 
-log "  Running BB git clone on #{reponame} to #{dest}"
-
 if "#{ssh_key}" != ""
+	Chef::Log.info "Creating SSH key"
 	RightScale::Repo::Ssh_key.new.create(ssh_key)
 end
 
@@ -29,12 +28,14 @@ directory dest do
 end
 
 execute 'git_clone' do
+	Chef::Log.info "Cloning #{reponame} into #{dest}"
 	command "git clone #{reponame} #{dest}"
 	action :run
 end
 
 ruby_block 'cleanup' do
 	block do
+		Chef::Log.info "Deleting SSH key"
 		RightScale::Repo::Ssh_key.new.delete
 	end
 end

1a5877eb566f0437252aedb87f9e496eb7296676
Matt Parrett <matt.parrett@gmail.com>
2012-09-24 17:46:25 -0500

    trying it like the mod_passenger example

--- cookbooks/bb_php/recipes/mod_geoip.rb
+++ cookbooks/bb_php/recipes/mod_geoip.rb
@@ -3,11 +3,9 @@ rightscale_marker :begin
 include_recipe "apache2"
 
 if platform?("redhat", "centos", "scientific", "fedora")
-#    package "geoip" do
-#        package_name "geoip"
-#        action :install
-#        notifies :run, resources(:execute => "generate-module-list"), :immediately
-#    end
+    package "mod_geoip" do
+        notifies :run, resources(:execute => "generate-module-list"), :immediately
+    end
     
 #    file "#{node[:apache][:dir]}/conf.d/geoip.conf" do
 #        action :delete
@@ -21,11 +19,11 @@ if platform?("redhat", "centos", "scientific", "fedora")
 		action :run
 	end
  
-#    template "#{node[:apache][:dir]}/geoip.conf" do
-#        source "geoip.conf.erb"
-#        notifies :restart, resources(:service => "apache2")
-#        mode 0644
-#    end
+    template "#{node[:apache][:dir]}/mods-available/geoip.load" do
+        source "geoip.load.erb"
+        notifies :restart, resources(:service => "apache2")
+        mode 0644
+    end
 end
 
 apache_module "geoip" do

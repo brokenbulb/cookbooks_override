70c7d67a5567d6734efc064cad1fe776a4c1fded
Matt Parrett <matt.parrett@gmail.com>
2012-09-25 11:38:33 -0500

    fix indents

--- cookbooks/bb_php/recipes/mod_geoip.rb
+++ cookbooks/bb_php/recipes/mod_geoip.rb
@@ -5,7 +5,7 @@ include_recipe "apache2"
 if platform?("redhat", "centos", "scientific", "fedora")
     package "mod_geoip" do
         notifies :run, resources(:execute => "generate-module-list"), :immediately
-		action :install
+        action :install
     end
     
     file "#{node[:apache][:dir]}/conf.d/geoip.conf" do
@@ -13,22 +13,22 @@ if platform?("redhat", "centos", "scientific", "fedora")
         backup false
     end
   
-	directory "/usr/local/share/GeoIP" do
-		Chef::Log.info "Create directory" 
-		recursive true
-		action :create
-	end
+    directory "/usr/local/share/GeoIP" do
+        Chef::Log.info "Create directory" 
+        recursive true
+        action :create
+    end
 
-	execute "copygeoipdat" do
-		Chef::Log.info "BB: Copy data" 
-		filename = ::File.join(::File.dirname(__FILE__), "..", "files", "centos-misc", "GeoIP.dat")
-		creates "/usr/local/share/GeoIP/GeoIP.dat"
-		command "mv " + filename + " /usr/local/share/GeoIP/"
-		action :run
-	end
+    execute "copygeoipdat" do
+        Chef::Log.info "BB: Copy data" 
+        filename = ::File.join(::File.dirname(__FILE__), "..", "files", "centos-misc", "GeoIP.dat")
+        creates "/usr/local/share/GeoIP/GeoIP.dat"
+        command "mv " + filename + " /usr/local/share/GeoIP/"
+        action :run
+    end
 
     template "#{node[:apache][:dir]}/mods-available/geoip.load" do
-		Chef::Log.info "BB: Template #{node[:apache][:dir]/mods-available/geoip.load"
+        Chef::Log.info "BB: Template #{node[:apache][:dir]/mods-available/geoip.load"
         source "geoip.load.erb"
         notifies :restart, resources(:service => "apache2")
         mode 0644

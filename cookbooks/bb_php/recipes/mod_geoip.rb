5b12adc0bcabb23c241a346da782b2cc19869e39
Matt Parrett <matt.parrett@gmail.com>
2012-09-25 11:30:32 -0500

    fix mod_geoip

--- cookbooks/bb_php/recipes/mod_geoip.rb
+++ cookbooks/bb_php/recipes/mod_geoip.rb
@@ -3,28 +3,32 @@ rightscale_marker :begin
 include_recipe "apache2"
 
 if platform?("redhat", "centos", "scientific", "fedora")
-#    package "mod_geoip" do
-#        notifies :run, resources(:execute => "generate-module-list"), :immediately
-#    end
+    package "mod_geoip" do
+        notifies :run, resources(:execute => "generate-module-list"), :immediately
+		action :install
+    end
     
-#    file "#{node[:apache][:dir]}/conf.d/geoip.conf" do
-#        action :delete
-#        backup false
-#    end
+    file "#{node[:apache][:dir]}/conf.d/geoip.conf" do
+        action :delete
+        backup false
+    end
   
 	directory "/usr/local/share/GeoIP" do
+		Chef::Log.info "Create directory" 
 		recursive true
 		action :create
 	end
 
 	execute "copygeoipdat" do
+		Chef::Log.info "BB: Copy data" 
 		filename = ::File.join(::File.dirname(__FILE__), "..", "files", "centos-misc", "GeoIP.dat")
 		creates "/usr/local/share/GeoIP/GeoIP.dat"
 		command "mv " + filename + " /usr/local/share/GeoIP/"
 		action :run
 	end
- 
+
     template "#{node[:apache][:dir]}/mods-available/geoip.load" do
+		Chef::Log.info "BB: Template #{node[:apache][:dir]/mods-available/geoip.load"
         source "geoip.load.erb"
         notifies :restart, resources(:service => "apache2")
         mode 0644
@@ -32,9 +36,5 @@ if platform?("redhat", "centos", "scientific", "fedora")
 end
 
 apache_module "geoip"
-#apache_module "geoip" do
-#	filename "geoip.so"
-#	conf false
-#end
 
 rightscale_marker :end

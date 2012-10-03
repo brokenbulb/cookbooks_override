ddcd42c12600ac96e27ae99c1959f83898f0570a
Matt Parrett <matt.parrett@gmail.com>
2012-10-03 15:29:55 -0500

    fixed typo

--- cookbooks/bb_php/recipes/open_ports.rb
+++ cookbooks/bb_php/recipes/open_ports.rb
@@ -13,10 +13,11 @@ rightscale_marker :begin
 include_recipe "iptables"
 include_recipe "sys_firewall"
 
-rule_ports = node[:bb_php][:open_ports].split(/, ?/)
+ports = node[:bb_php][:open_ports].split(/, ?/)
 ports.each do |port|
     raise "Invalid port specified: #{port}. Valid range 1-65536" unless port > 0 and port <= 65536
     sys_firewall port do
+      Chef::Log.info "BB: Opening port #{port}"
       ip_addr "255.0.0.0"
     end
 end
